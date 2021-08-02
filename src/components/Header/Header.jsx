092bfa469cba2ff52dfb2ffc32c63358e1bb8de9
Kalan Prudhomme <cleaver9@gmail.com>
2021-08-02 09:26:27 -0700

    added header, styling

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './Header.css';
+
+const Header = () => {
+  return (
+    <header className={styles.Header}>
+      <h1>
+                RESTy the RESTful Returner
+      </h1>
+    </header>
+  );
+};
+
+export default Header;
